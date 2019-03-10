4cef5f8d8a8b82425a68542634bc59e108ad5d74
Alex <pachot@gmail.com>
2019-03-10 16:16:42 -0400

    Material Design Lite

--- vues/entete.php
+++ vues/entete.php
@@ -10,13 +10,16 @@
 		<meta name="description" content="Un petit verre de vino">
 		<meta name="author" content="Jonathan Martel (jmartel@cmaisonneuve.qc.ca)">
 
-		<link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
+		<!-- <link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
-		<link rel="stylesheet" href="./css/main.css" type="text/css" media="screen">
+		<link rel="stylesheet" href="./css/main.css" type="text/css" media="screen"> -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link rel="stylesheet" href="./css/material.css">
 		<base href="<?php echo BASEURL; ?>">
 		<!--<script src="./js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>-->
-		<script src="./js/plugins.js"></script>
-		<script src="./js/main.js"></script>
+		<!-- <script src="./js/plugins.js"></script>
+		<script src="./js/main.js"></script> -->
+		<script defer src="./js/material.js"></script>
 	</head>
 	<body >
 		<header>
