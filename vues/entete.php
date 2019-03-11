978427dd5a17900b3ec140f5b115a360ecebb829
charefeddine <48103950+charefeddine@users.noreply.github.com>
2019-03-11 17:35:47 -0400

    reglage boutons ajouter et boire

--- vues/entete.php
+++ vues/entete.php
@@ -25,8 +25,9 @@
 
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-		<!-- <link rel="stylesheet" href="./css/material.css"> -->
+		<link rel="stylesheet" href="./css/material.css">
 		<link rel="stylesheet" href="./css/styles.css">
+		<script src="./js/main.js"></script>
 	</head>
 	<body >
 		<header>
