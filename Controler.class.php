4c39067bff7eb8170c405cb5f3920b9911af7d42
Alex <pachot@gmail.com>
2019-03-11 17:52:57 -0400

    Controleur : ajout case Alex (test MDL)

--- Controler.class.php
+++ Controler.class.php
@@ -39,6 +39,9 @@ class Controler
 				case 'boireBouteilleCellier':
 					$this->boireBouteilleCellier();
 					break;
+				case 'alex':
+					$this->alex();
+					break;
 				default:
 					$this->accueil();
 					break;
@@ -55,6 +58,12 @@ class Controler
 			include("vues/pied.php");
 		}
 		
+		private function alex()
+		{
+			$bouteille_cellier = new Bouteille();
+			$donnees = $bouteille_cellier->obtenir_liste_bouteilles_cellier(1);
+			include("mdlt/index.html");
+		}
 
 		private function listeBouteille()
 		{
