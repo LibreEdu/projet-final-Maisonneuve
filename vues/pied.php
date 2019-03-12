49c4c247f6c7cb2f74ba3c97eae803eb6be0ae9f
Alex <pachot@gmail.com>
2019-03-12 17:17:56 -0400

    modif Fatemeh Entête et pied de page

--- vues/pied.php
+++ vues/pied.php
@@ -1,25 +1,25 @@
-        <footer class="mdl-mega-footer">
+<footer class="mdl-mega-footer">
           <div class="mdl-mega-footer--middle-section">
             <div class="mdl-mega-footer--drop-down-section">
               <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-              <h1 class="mdl-mega-footer--heading">Features</h1>
-              <ul class="mdl-mega-footer--link-list">
+              <h1 class="mdl-mega-footer--heading">Mon cellier</h1>
+              <!-- <ul class="mdl-mega-footer--link-list">
                 <li><a href="#">About</a></li>
                 <li><a href="#">Terms</a></li>
                 <li><a href="#">Partners</a></li>
                 <li><a href="#">Updates</a></li>
-              </ul>
+              </ul>-->
             </div>
             <div class="mdl-mega-footer--drop-down-section">
               <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-              <h1 class="mdl-mega-footer--heading">Details</h1>
-              <ul class="mdl-mega-footer--link-list">
+              <h1 class="mdl-mega-footer--heading">Ajouter bouteille</h1>
+              <!--<ul class="mdl-mega-footer--link-list">
                 <li><a href="#">Spec</a></li>
                 <li><a href="#">Tools</a></li>
                 <li><a href="#">Resources</a></li>
-              </ul>
+              </ul>-->
             </div>
-            <div class="mdl-mega-footer--drop-down-section">
+            <!--<div class="mdl-mega-footer--drop-down-section">
               <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
               <h1 class="mdl-mega-footer--heading">Technology</h1>
               <ul class="mdl-mega-footer--link-list">
@@ -29,8 +29,8 @@
                 <li><a href="#">Products</a></li>
                 <li><a href="#">Contracts</a></li>
               </ul>
-            </div>
-            <div class="mdl-mega-footer--drop-down-section">
+            </div>-->
+            <!--<div class="mdl-mega-footer--drop-down-section">
               <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
               <h1 class="mdl-mega-footer--heading">FAQ</h1>
               <ul class="mdl-mega-footer--link-list">
@@ -43,17 +43,16 @@
           <div class="mdl-mega-footer--bottom-section">
             <div class="mdl-logo">
               More Information
-            </div>
+            </div
             <ul class="mdl-mega-footer--link-list">
               <li><a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a></li>
-              <li><a href="#">Help</a></li>
-              <li><a href="#">Privacy and Terms</a></li>
+              <li><a href="#">Se deconnecter</a></li>
             </ul>
-          </div>
+          </div>>-->
         </footer>
       </main>
     </div>
-    <a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/text-only/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">View Source</a>
+    <!--<a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/text-only/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">View Source</a>-->
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   </body>
 </html>
