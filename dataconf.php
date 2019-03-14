00818f8ace7225a8ef5d82295b20ef4cf504803d
Alex <pachot@gmail.com>
2019-03-13 21:55:29 -0400

    Sauvegarde du Sprint 0

--- dataconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// Remplir et renommer dataconf.php
-	define("BASEURL", "");
-
-	define('HOTE', 'localhost');
-	define('UTILISATEUR', 'root');
-	define('MOTDEPASSE', '');
-	define('BASEDEDONNEES', 'vino');
