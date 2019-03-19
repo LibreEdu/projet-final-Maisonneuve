9a9de475e4bb9a8e9a00959c579bb57a46b1baef
Alexandre Pachot <pachot@gmail.com>
2019-03-19 15:26:09 -0400

    vue cellier : suppression data-quantite = '' (vide !)

--- privee/vues/cellier.php
+++ privee/vues/cellier.php
@@ -3,10 +3,8 @@
 			<?php foreach ($data['bouteilles'] as $bouteille): ?>
 			<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
 				<header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white">
-					<div class='bouteille' data-quantite=''>
-						<div class='img'>
-							<img src='https://s7d9.scene7.com/is/image/SAQ/<?php echo $bouteille->code_saq; ?>_is?$saq-rech-prod-gril$'>
-						</div>
+					<div class='img'>
+						<img src='https://s7d9.scene7.com/is/image/SAQ/<?php echo $bouteille->code_saq; ?>_is?$saq-rech-prod-gril$'>
 					</div>
 				</header>
 				<div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
