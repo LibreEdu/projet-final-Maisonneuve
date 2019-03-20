9eab9cb3d442a2b00c3162af90f12ef52ecf70a9
Alexandre Pachot <pachot@gmail.com>
2019-03-20 07:18:47 -0400

    config : commentaires + modification nom fonction autoload

--- prive/config.renommer.php
+++ prive/config.renommer.php
@@ -1,16 +1,25 @@
 <?php
 	// Définition des constantes
+
+	// Pour le chargement automatique des classes
 	define("RACINE", $_SERVER["DOCUMENT_ROOT"] . '/vino/prive/');
+
+	// Pour le chargement du CSS, JS et images
+	// Ce qui doit être directement accessible par le navigateur
 	define("BASEURL", 'http://127.0.0.1/vino/www/');
-	define("DBTYPE", 'mysql');
+
+
+	// Pour la connexion à la base de données
+	define("DBTYPE", 'mysql'); // mysql ou oracle
 	define("HOST", 'localhost');
 	define("DBNAME", 'vino');
 	define("CHARSET", 'utf8mb4');
 	define("USER", 'root');
 	define("PWD", '');
 	
+
 	// Fonction d’autoload
-	function my_autoloader($classe)
+	function chargement_automatique_des_classes($classe)
 	{
 		$repertoires = array(
 			RACINE . 'controleurs/',
@@ -18,17 +27,18 @@
 			RACINE . 'vues/'
 		);
 
-		foreach($repertoires as $rep)
+		foreach($repertoires as $repertoire)
 		{
-			if(file_exists($rep . $classe . '.php'))
+			if(file_exists($repertoire . $classe . '.php'))
 			{
-				require_once($rep . $classe . '.php');
+				require_once($repertoire . $classe . '.php');
 				return;
 			}
 		}
 	}
 
-	spl_autoload_register('my_autoloader');
+	// Enregistre une fonction en tant qu’implémentation de __autoload()
+	spl_autoload_register('chargement_automatique_des_classes');
 
 
 	// Action par défaut
