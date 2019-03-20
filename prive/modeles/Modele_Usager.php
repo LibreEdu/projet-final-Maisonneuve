ca5c018ed4703930b5a090b72e543a8e285c7a05
charefeddine <48103950+charefeddine@users.noreply.github.com>
2019-03-20 14:54:45 -0400

    Gestion de l'inscription

--- prive/modeles/Modele_Usager.php
+++ prive/modeles/Modele_Usager.php
@@ -69,6 +69,85 @@
 			$lUsager = $resultat->fetch();
 			return $lUsager;
 		}
+
+
+		 public function sauvegarde(Usager $lUsager)
+        {
+
+		$query = "INSERT INTO " . $this->getTableName() . " (courriel, admin, actif, nom, prenom, hash) VALUES (?, ?, ?, ?, ?, ?)";
+				$donnees = array($lUsager->courriel, $lUsager->admin, $lUsager->actif, $lUsager->nom, $lUsager->prenom, $lUsager->hash);
+				return $this->requete($query, $donnees);
+
+		}
+
+		 public function verif(){
+        
+         if(empty($_POST['pseudo']))
+					    {
+					    	$message = 'Le champ Courriel est vide';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Le champ Pseudo est vide.";
+					    } 
+					    elseif(!preg_match("#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#",$_POST['pseudo']))
+					    {
+					    	$message = 'le format devra réspecter celui du courriel.';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Le Pseudo doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
+					    }
+
+					    elseif(!preg_match("#^[a-z0-9]+$#",$_POST['nom']))
+					    {
+					    	$message = 'Le Nom ne doit pas contenir de chiffres';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Le Pseudo doit être renseigné sans chiffres ni caractères spéciaux.";
+					    }
+
+					    elseif(!preg_match("#^[a-z0-9]+$#",$_POST['prenom']))
+					    {
+					    	$message = 'Le Prénom ne doit pas contenir de chiffres';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Le Prénom doit être renseigné sans chiffres ni caractères spéciaux.";
+					    }   
+					  //   elseif(strlen($_POST['pseudo'])>25)
+					  //   {
+					  //   	$message = 'Le pseudo est trop long, il dépasse 25 caractères.';
+								
+							// $donnees['erreurs'] = $message;
+					  //      // echo "Le pseudo est trop long, il dépasse 25 caractères.";
+					  //   } 
+					    elseif(empty($_POST['mdp']))
+					    {//le champ mot de passe est vide
+					    	$message = 'Le champ Mot de passe est vide.';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Le champ Mot de passe est vide.";
+					    }
+					    elseif(strlen($_POST['mdp'])>12|| strlen($_POST['mdp'])<5)
+					    {
+					    	$message = 'Le mot de passe doit contenir entre 6 à 12 caracteres maximum.';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Le mot de passe doit contenir entre 6 à 12 caracteres maximum.";
+					    }
+					    elseif (($_POST['mdp']) != ($_POST['mdp2']) ) 
+					    {
+					      	$message = 'Les mots de passe doivent être identique.';
+								
+							$donnees['erreurs'] = $message;
+					    }  
+					    elseif(($modeleUsager->obtenirUsager($_REQUEST['pseudo'])))
+					    {//on vérifie que ce pseudo n'est pas déjà utilisé par un autre membre
+							$message = 'Ce pseudo est déjà utilisé.';
+								
+							$donnees['erreurs'] = $message;
+					        //echo "Ce pseudo est déjà utilisé.";
+					    } 
+        
+    }
 		
 		
 	}
