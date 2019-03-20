d6f66b0c014343347fa1f47e8e9865ab6770a7df
Alexandre Pachot <pachot@gmail.com>
2019-03-20 04:05:27 -0400

    Commentaires routeur

--- prive/controleurs/Routeur.php
+++ prive/controleurs/Routeur.php
@@ -1,44 +1,51 @@
 <?php
- // Ouverture de la session
-    session_start();
+	// Ouverture de la session
+	session_start();
 
-    class Routeur
-    {
-        public static function route()
-        {
-            //obtenir la chaine de la requête
-            $chaineRequete = $_SERVER["QUERY_STRING"];
-            $posEperluette = strpos($chaineRequete, "&");
-            $controleur = "";
-            if($posEperluette === false)
-                $controleur = $chaineRequete;
-            else
-                $controleur = substr($chaineRequete, 0, $posEperluette);
-            
-            //si aucun contrôleur n'a été spécifié, mettre un contrôleur par défaut
-            if($controleur == "")
-                $controleur = "Bouteille";
-            
-            //on a déterminé le controleur
-            //on en crée une instance si la classe existe
-            $classe = "Controleur_" . $controleur;            
-          
-            if(class_exists($classe))
-            {
-                //déclaration du controleur et traitement de la requête...
-                $objetControleur = new $classe;
-                
-                if($objetControleur instanceof BaseControleur)
-                {
-                    $objetControleur->traite($_REQUEST);
-                }
-                else
-                    trigger_error("Controleur invalide.");
-            }
-            else
-            {
-                trigger_error("Erreur 404! Le controleur $controleur n'existe pas.");
-            }
-        }
-    }
-?>
+	class Routeur
+	{
+		public static function route()
+		{
+			// Récupére la chaine de caractères de l’URL ou des données du formulaire
+			$chaineRequete = $_SERVER['QUERY_STRING'];
+
+			// Cherche la position de la première occurrence de & dans la chaine de caractères
+			$posEperluette = strpos($chaineRequete, '&');
+
+			// Initialise le controleur
+			$controleur = '';
+
+			// Récupère le controleur
+			if($posEperluette === false)
+				$controleur = $chaineRequete;
+			else
+				$controleur = substr($chaineRequete, 0, $posEperluette);
+
+			// Si aucun contrôleur n’est spécifié, mettre le contrôleur par défaut
+			if($controleur == "")
+				$controleur = "Bouteille";
+
+			// Détermin la classe du controleur
+			$classe = "Controleur_" . $controleur;
+
+			// Si la classe existe
+			if(class_exists($classe))
+			{
+				// Crée une nouvelle instance de la classe
+				$objetControleur = new $classe;
+
+				// Si l’objet est instancié d’une classe qui hérite de BaseControleur
+				if($objetControleur instanceof BaseControleur)
+				{
+					// Traite la requête
+					$objetControleur->traite($_REQUEST);
+				}
+				else
+					trigger_error("Controleur invalide.");
+			}
+			else
+			{
+				trigger_error("Erreur 404! Le controleur $controleur n’existe pas.");
+			}
+		}
+	}
