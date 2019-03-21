38100b1bac8ca5a760050f6daa076fe99dd4aa50
fatemeh <f.homatash@gmail.com>
2019-03-21 11:29:44 -0400

    regler le button supprimer cellier

--- prive/controleurs/Controleur_ucellier.php
+++ prive/controleurs/Controleur_ucellier.php
@@ -39,12 +39,13 @@
 
 				case 'supprimerCellier':
 					$body = json_decode(file_get_contents('php://input'));
+					//var_dump($body);die;
 					$modeleCellier = $this->getDAO('Cellier');
 					$modeleCellier->supprimer_par_id($body->id);
-					$donnees['celliers'] = $modeleCellier->obtenir_par_id(1);
-					$this->afficheVue('modeles/en-tete');
-					$this->afficheVue('cellier/liste', $donnees);
-					$this->afficheVue('modeles/bas-de-page');
+					//$donnees['celliers'] = $modeleCellier->obtenir_par_id(1);
+					//$this->afficheVue('modeles/en-tete');
+					//$this->afficheVue('cellier/liste', $donnees);
+					//	$this->afficheVue('modeles/bas-de-page');
 					break;
 
 				default :
