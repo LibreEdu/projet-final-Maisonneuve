a5f466386ee9bcff5408f1048bf8cedf4eb011d0
Alexandre Pachot <pachot@gmail.com>
2019-03-21 16:42:18 -0400

    Séparation en-tête / menu usager / menu admin

---
+++ prive/vues/modeles/menu-usager.php
@@ -0,0 +1,20 @@
+	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_purple.min.css">
+	<link rel="stylesheet" href="<?php echo BASEURL?>css/vino.css">
+</head>
+<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+	<header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+		<div class="mdl-layout--large-screen-only mdl-layout__header-row">
+		</div>
+		<div class="mdl-layout--large-screen-only mdl-layout__header-row">
+		<h3>Vino &amp; plaisir</h3>
+		</div>
+		<div class="mdl-layout--large-screen-only mdl-layout__header-row">
+		</div>
+		<div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
+			<a href="index.php?bouteille" class="mdl-layout__tab is-active">Mon cellier</a>
+			<a href="index.php?bouteilleSaq" class="mdl-layout__tab is-active">Bouteille SAQ</a>
+			<a href="index.php?uCellier" class="mdl-layout__tab is-active">Celliers</a>
+			<a href="index.php?bouteille&action=ajouter-form" class="mdl-layout__tab is-active">Ajouter bouteille</a>
+		</div>
+	</header>
