bd081d9640e84d0dd16bd4ffa6154fdf3ad933c9
Alexandre Pachot <pachot@gmail.com>
2019-03-23 09:58:07 -0400

    class Controleur
    Indentation

--- prive/controleurs/Controleur.php
+++ prive/controleurs/Controleur.php
@@ -1,46 +1,46 @@
 <?php
-	abstract class Controleur
+abstract class Controleur
+{
+	// La fonction qui sera appelée par le routeur
+	public abstract function traite(array $params);
+	
+	protected function afficheVue($nomVue, $donnees = null)
 	{
-		// La fonction qui sera appelée par le routeur
-		public abstract function traite(array $params);
+		$cheminVue = RACINE . "vues/" . $nomVue . ".php";
 		
-		protected function afficheVue($nomVue, $donnees = null)
+		if(file_exists($cheminVue))
 		{
-			$cheminVue = RACINE . "vues/" . $nomVue . ".php";
-			
-			if(file_exists($cheminVue))
-			{
-				include_once($cheminVue);
-			}
-			else
-			{
-				trigger_error("Erreur 404! La vue $nomVue n'existe pas.");
-			}
+			include_once($cheminVue);
 		}
-
-		protected function getDAO($nomModele)
+		else
 		{
-			$classe = "Modele_" . $nomModele;
-			if(class_exists($classe))
-			{
-				// On fait une connexion à la BD
-				$connexion = DBFactory::getDB(DBTYPE, HOST, DBNAME, CHARSET, USER, PWD);
-				
-				// On crée une instance de la classe Modele_$classe
-				$objetModele = new $classe($connexion);
-
-				if($objetModele instanceof BaseDAO)
-				{
-					return $objetModele;
-				}
-				else
-				{
-					trigger_error("Modèle invalide.");
-				}
-			}
-			else
-			{
-				trigger_error("La classe $classe est invalide.");
-			}
+			trigger_error("Erreur 404! La vue $nomVue n'existe pas.");
 		}
 	}
+
+	protected function getDAO($nomModele)
+	{
+		$classe = "Modele_" . $nomModele;
+		if(class_exists($classe))
+		{
+			// On fait une connexion à la BD
+			$connexion = DBFactory::getDB(DBTYPE, HOST, DBNAME, CHARSET, USER, PWD);
+			
+			// On crée une instance de la classe Modele_$classe
+			$objetModele = new $classe($connexion);
+
+			if($objetModele instanceof BaseDAO)
+			{
+				return $objetModele;
+			}
+			else
+			{
+				trigger_error("Modèle invalide.");
+			}
+		}
+		else
+		{
+			trigger_error("La classe $classe est invalide.");
+		}
+	}
+}
