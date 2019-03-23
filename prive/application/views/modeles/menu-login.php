9aa11106278e72da0e7d087c2487aee964d115f4
Alexandre Pachot <pachot@gmail.com>
2019-03-23 08:19:10 -0400

    Vues de la branche master

---
+++ prive/application/views/modeles/menu-login.php
@@ -0,0 +1,12 @@
+		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_purple.min.css">
+		<link rel="stylesheet" href="<?php echo BASEURL?>css/vino.css">
+	</head>
+	<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+		<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+			<header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+				<div class="mdl-layout--large-screen-only mdl-layout__header-row"></div>
+				<div class="mdl-layout--large-screen-only mdl-layout__header-row">
+					<h3>Vino &amp; plaisir</h3>
+				</div>
+				<div class="mdl-layout--large-screen-only mdl-layout__header-row"></div>
+			</header>
