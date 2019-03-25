ce7ee60da58b2d6c7e0b3d1db58a79f6a60e3b72
Alexandre Pachot <pachot@gmail.com>
2019-03-25 06:30:32 -0400

    Classe BaseDeDonnees

---
+++ prive/modeles/BaseDeDonnees.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+	class BaseDeDonnees
+	{
+		public static function obtenirBD($typeBD, $host, $dbName, $charset, $user, $pwd)
+		{
+			if($typeBD == 'mysql')
+			{
+				$BD = new PDO("mysql:host=$host;dbname=$dbName;charset=$charset", $user, $pwd);
+			}
+			else if($typeBD == 'oracle')
+			{
+				$BD = new PDO("oci:host=$host;dbname=$dbName;charset=$charset", $user, $pwd);		
+			}
+			else
+			{
+				trigger_error('Le type de BD spécifié n’est pas supporté.');
+			}
+			$BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $BD;
+		}
+	}
+	
+?>
+
