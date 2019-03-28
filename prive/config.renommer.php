f2c8a11c97ef7cb4e7da58c6ae77873389b8c218
Alexandre Pachot <pachot@gmail.com>
2019-03-28 07:01:30 -0400

    Fichier de configuration

--- prive/config.renommer.php
+++ prive/config.renommer.php
@@ -1,92 +1,92 @@
 <?php
-	/**
-	 * @package  Vino
-	 */
-
-	 /**
-	 * Racine du répertoire PHP.
-	 * Pour le chargement automatique des classes.
-	 */
-	define("RACINE", $_SERVER["DOCUMENT_ROOT"] . '/../prive/');
-
+/**
+ * @package  Vino
+ */
 
 	/**
-	 * URL de la racine du site.
-	 * Cette URL est nécessaire pour le chargement du CSS, JS et images :
-	 * en bref, tout ce qui doit être directement accessible par le navigateur.
-	 */
-	define("BASEURL", 'http://127.0.0.1/');
+ * Racine du répertoire PHP.
+ * Pour le chargement automatique des classes.
+ */
+define('RACINE', $_SERVER['DOCUMENT_ROOT'] . '/../prive/');
 
 
-	/**
-	 * Type de base de données.
-	 * Nous avons le choix entre mysql et oracle.
-	 */
-	define("DBTYPE", 'mysql'); // mysql ou oracle
+/**
+ * URL de la racine du site.
+ * Cette URL est nécessaire pour le chargement du CSS, JS et images :
+ * en bref, tout ce qui doit être directement accessible par le navigateur.
+ */
+define('BASEURL', 'http://127.0.0.1/');
 
 
-	/**
-	 * Adresse du serveur contenant la base de données.
-	 */
-	define("HOST", 'localhost');
+/**
+ * Type de base de données.
+ * Nous avons le choix entre mysql et oracle.
+ */
+define('DBTYPE', 'mysql'); // mysql ou oracle
 
 
-	/**
-	 * Nom de la base de données.
-	 */
-	define("DBNAME", 'vino');
+/**
+ * Adresse du serveur contenant la base de données.
+ */
+define('HOST', 'localhost');
 
 
-	/** 
-	 * Jeu de caractères utilisé pour l’encodage des données.
-	 */
-	define("CHARSET", 'utf8mb4');
+/**
+ * Nom de la base de données.
+ */
+define('DBNAME', 'vino');
 
 
-	/** 
-	 * Nom d’utilisateur de la base de données.
-	 */
-	define("USERNAME", 'root');
+/** 
+ * Jeu de caractères utilisé pour l’encodage des données.
+ */
+define('CHARSET', 'utf8mb4');
 
 
-	/** 
-	 * Mot de passe de la base de données.
-	 */
-	define("PASSWD", '');
-	
+/** 
+ * Nom d’utilisateur de la base de données.
+ */
+define('USERNAME', 'root');
 
-	/**
-	 * Fonction de chargement automatique des classes.
-	 * 
-	 * @param object $classe Classe que l’on veut charger dynamiquement
-	 * 
-	 * @return void
-	 */
-	function chargement_automatique_des_classes($classe)
+
+/** 
+ * Mot de passe de la base de données.
+ */
+define('PASSWD', '');
+
+
+/**
+ * Fonction de chargement automatique des classes.
+ * 
+ * @param object $classe Classe que l’on veut charger dynamiquement
+ * 
+ * @return void
+ */
+function chargement_automatique_de_la_classe($classe)
+{
+	$repertoires = array(
+		RACINE . 'controleurs/',
+		RACINE . 'modeles/',
+		RACINE . 'vues/'
+	);
+
+	foreach($repertoires as $repertoire)
 	{
-		$repertoires = array(
-			RACINE . 'controleurs/',
-			RACINE . 'modeles/',
-			RACINE . 'vues/'
-		);
-
-		foreach($repertoires as $repertoire)
+		if(file_exists($repertoire . $classe . '.php'))
 		{
-			if(file_exists($repertoire . $classe . '.php'))
-			{
-				require_once($repertoire . $classe . '.php');
-				return;
-			}
+			require_once($repertoire . $classe . '.php');
+			return;
 		}
 	}
+}
 
 
-	// Enregistre une fonction en tant qu’implémentation de __autoload()
-	spl_autoload_register('chargement_automatique_des_classes');
+// Enregistre une fonction en tant qu’implémentation de __autoload()
+spl_autoload_register('chargement_automatique_de_la_classe');
 
 
-	// Action par défaut
-	if (empty($_REQUEST['action']))
-	{
-		$_REQUEST['action'] = 'index';
-	}
+// Action par défaut
+if (empty($_REQUEST['action']))
+{
+	$_REQUEST['action'] = 'index';
+}
