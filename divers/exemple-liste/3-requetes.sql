f9ac7f0a69b7f1bca569355c3264adba6ef7ed89
Alexandre Pachot <pachot@gmail.com>
2019-03-29 14:33:55 -0400

    exemple liste

---
+++ divers/exemple-liste/3-requetes.sql
@@ -0,0 +1,4 @@
+INSERT INTO `voiture` (`id_voiture`, `marque`, `modele`) VALUES (NULL, 'Volkswagen', 'Golf');
+INSERT INTO `voiture` (`id_voiture`, `marque`, `modele`) VALUES (NULL, 'Volkswagen', 'Polo');
+INSERT INTO `voiture` (`id_voiture`, `marque`, `modele`) VALUES (NULL, 'Honda', 'Civic');
+INSERT INTO `voiture` (`id_voiture`, `marque`, `modele`) VALUES (NULL, 'Honda', 'Accord');
