2426872860725d6d49b68fbad7b1c11dbe5201d7
Alexandre Pachot <pachot@gmail.com>
2019-03-30 04:26:13 -0400

    Modèle Bouteille SAQ

--- prive/modeles/Modele_Bouteille_SAQ.php
+++ prive/modeles/Modele_Bouteille_SAQ.php
@@ -56,7 +56,7 @@ class Modele_Bouteille_SAQ extends Modele
 	}
 
 	public function recherche($id_cellier, $recherchePar, $valeur, $operation)
-	{		
+	{
 		$listeBouteilles = Array();
 		$valeur = preg_replace('/\*/','%' , $valeur);
 		if ($recherchePar=='nom' ||  $recherchePar=='type' || $recherchePar=='pays') {
