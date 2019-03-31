00e123b2864d1698cb9db9a53a8dbaa9fe623306
Alexandre Pachot <pachot@gmail.com>
2019-03-30 21:04:12 -0400

    Classe Log

---
+++ prive/controleurs/Log.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Permet d’écrire des fichiers de log.
+ *
+ * @package  Vino
+ * @author   Alexandre Pachot
+ * @version  1.0
+ */
+class Log
+{
+	/**
+	 * @var string $_nom_fichier Nom du fichier de log ainsi que de son répertoire.
+	 */
+	private $_nom_fichier;
+
+
+	/**
+	 * Initialisation des attributs.
+	 * 
+	 * @param string $nom_fichier Nom du fichier de log
+	 * 
+	 * @return void
+	 */
+	public function __construct($nom_fichier)
+	{
+		$this->_nom_fichier = RACINE . 'logs/' . $nom_fichier . '.log';
+	}
+
+
+	/**
+	 * Écriture dans le fichier de log.
+	 * 
+	 * @param string $message Message que l’on veut écrire dans le fichier de log.
+	 * 
+	 * @return void
+	 */
+	public function ecriture($message)
+	{
+		$date = new DateTime();
+		$date = $date->format('y:m:d h:i:s');
+		error_log($date . '  ' . $message, 3, $this->_nom_fichier);
+	}
+}
