3db4e1391d43b749e21bac7049891e6612322e64
Alexandre Pachot <pachot@gmail.com>
2019-03-31 07:14:50 -0400

    CSS : On recommence à 0  -- paragraphes ++ indentation

--- prive/vues/admin/en-tete.php
+++ prive/vues/admin/en-tete.php
@@ -11,50 +11,61 @@
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/vino-admin.css">
 	</head>
 	<body>
-		<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-			<div class="vino-en-tete mdl-layout__header mdl-layout__header--waterfall">
-				<div class="mdl-layout__header-row">
-					<span class="vino-titre mdl-layout-title">
-						<img class="vino-logo-image" src="<?php echo base_url(); ?>images/vino-logo.png">
-					</span>
-					<!-- Ajoute un espace afin d’aligner la barre de navigation à droite, version ordinateur -->
-					<div class="mdl-layout-spacer"></div>
-					<!-- Barre de navigation -->
-					<div class="vino-navigation-conteneur">
-						<nav class="vino-navigation mdl-navigation">
-							<a class="mdl-navigation__link mdl-typography--text-uppercase" href="<?php echo site_url('login&action=logout'); ?>">Déconnexion</a>
-						</nav>
-					</div>
-					<span class="vino-logo-cell mdl-layout-title">
-						<img class="vino-logo-image" src="<?php echo base_url(); ?>images/vino-logo.png">
-					</span>
-				</div>
-			</div>
-			<div class="vino-lateral mdl-layout__drawer">
-				<span class="mdl-layout-title">
-					<img class="vino-logo-image" src="images/vino-logo-blanc.png">
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		<div class="android-header mdl-layout__header mdl-layout__header--waterfall">
+			<div class="mdl-layout__header-row">
+				<span class="android-title mdl-layout-title">
+					<img class="android-logo-image" src="images/android-logo.png">
 				</span>
-				<nav class="mdl-navigation">
-					<a class="mdl-navigation__link" href="<?php echo site_url('login&action=logout'); ?>">Déconnexion</a>
-				</nav>
-			</div>
-			<div class="android-content mdl-layout__content">
-				<a name="haut_de_page"></a>
-				<div class="vino-section-bienvenue mdl-typography--text-center">
-					<div class="bienvenue-police vino-bonjour">Bonjour <?php echo $donnees['prenom'] ?></div>
-					<div class="bienvenue-police vino-bienvenue">Bienvenue à Vino administration</div>
-					<a href="#importation">
-						<button class="vino-fab mdl-button mdl-button--colored mdl-js-button mdl-button--fab mdl-js-ripple-effect">
-							<i class="material-icons">expand_more</i>
-						</button>
-					</a>
+				<!-- Add spacer, to align navigation to the right in desktop -->
+				<div class="android-header-spacer mdl-layout-spacer"></div>
+				<!-- Navigation -->
+				<div class="android-navigation-container">
+					<nav class="android-navigation mdl-navigation">
+						<a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Phones</a>
+					</nav>
 				</div>
-				<div class="vino-importation mdl-typography--text-center">
-					<a name="importation"></a>
-					<div class="mdl-typography--display-1-color-contrast">Importation SAQ</div>
-				</div>
+				<span class="android-mobile-title mdl-layout-title">
+					<img class="android-logo-image" src="images/android-logo.png">
+				</span>
+				<button class="android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
+					<i class="material-icons">more_vert</i>
+				</button>
+				<ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect" for="more-button">
+					<li class="mdl-menu__item">5.0 Lollipop</li>
+					<li class="mdl-menu__item">4.4 KitKat</li>
+					<li disabled class="mdl-menu__item">4.3 Jelly Bean</li>
+					<li class="mdl-menu__item">Android History</li>
+				</ul>
 			</div>
-			<footer class="android-footer mdl-mega-footer">
+		</div>
+		<div class="android-drawer mdl-layout__drawer">
+			<span class="mdl-layout-title">
+				<img class="android-logo-image" src="images/android-logo-white.png">
+			</span>
+			<nav class="mdl-navigation">
+				<a class="mdl-navigation__link" href="">Phones</a>
+			</nav>
+		</div>
+		<div class="android-content mdl-layout__content">
+		<a name="top"></a>
+		<div class="android-be-together-section mdl-typography--text-center">
+			<div class="logo-font android-slogan">be together. not the same.</div>
+			<div class="logo-font android-sub-slogan">welcome to android... be yourself. do your thing. see what's going on.</div>
+			<div class="logo-font android-create-character">
+				<a href=""><img src="images/andy.png"> create your android character</a>
+			</div>
+			<a href="#screens">
+				<button class="android-fab mdl-button mdl-button--colored mdl-js-button mdl-button--fab mdl-js-ripple-effect">
+					<i class="material-icons">expand_more</i>
+				</button>
+			</a>
+		</div>
+		<div class="android-screen-section mdl-typography--text-center">
+			<a name="screens"></a>
+			<div class="mdl-typography--display-1-color-contrast">Powering screens of all sizes</div>
+		</div>
+		<footer class="android-footer mdl-mega-footer">
 				<div class="mdl-mega-footer--top-section">
 					<div class="mdl-mega-footer--right-section">
 						<a class="mdl-typography--font-light" href="#haut_de_page">
