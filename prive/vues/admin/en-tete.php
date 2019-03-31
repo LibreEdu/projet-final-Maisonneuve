52fc43ff066d9e529c749273bb73f3a4b753d5db
Alexandre Pachot <pachot@gmail.com>
2019-03-30 21:09:19 -0400

    Page admin CSS 21:09

---
+++ prive/vues/admin/en-tete.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="Page d’administration de Vino">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+		<title>Vino admin</title>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/material.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/vino-admin.css">
+	</head>
+	<body>
+		<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+			<div class="vino-barre-navigation mdl-layout__header mdl-layout__header--waterfall">
+				<div class="mdl-layout__header-row">
+					<span class="android-title mdl-layout-title">
+						<img class="vino-logo-image" src="<?php echo base_url(); ?>images/vino-logo.png">
+					</span>
+					<!-- Ajoute un espace afin d’aligner la barre de navigation à droite, version ordinateur -->
+					<div class="android-header-spacer mdl-layout-spacer"></div>
+					<!-- Barre de navigation -->
+					<div class="android-navigation-container">
+						<nav class="android-navigation mdl-navigation">
+							<a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Déconnexion</a>
+						</nav>
+					</div>
+					<span class="vino-logo-cell mdl-layout-title">
+						<img class="vino-logo-image" src="<?php echo base_url(); ?>images/vino-logo.png">
+					</span>
+				</div>
+			</div>
+			<div class="android-drawer mdl-layout__drawer">
+				<span class="mdl-layout-title">
+					<img class="vino-logo-image" src="images/vino-logo-blanc.png">
+				</span>
+				<nav class="mdl-navigation">
+					<a class="mdl-navigation__link" href="">Déconnexion</a>
+				</nav>
+			</div>
+			<div class="android-content mdl-layout__content">
+				<a name="haut_de_page"></a>
+				<div class="android-be-together-section mdl-typography--text-center">
+					<div class="logo-font android-slogan">Bonjour Alexandre</div>
+					<div class="logo-font android-sub-slogan">Bienvenue à Vino administration</div>
+					<a href="#importation">
+						<button class="vino-fab mdl-button mdl-button--colored mdl-js-button mdl-button--fab mdl-js-ripple-effect">
+							<i class="material-icons">expand_more</i>
+						</button>
+					</a>
+				</div>
+				<div class="android-screen-section mdl-typography--text-center">
+					<a name="importation"></a>
+					<div class="mdl-typography--display-1-color-contrast">Importation SAQ</div>
+				</div>
+			</div>
+			<footer class="android-footer mdl-mega-footer">
+				<div class="mdl-mega-footer--top-section">
+					<div class="mdl-mega-footer--right-section">
+						<a class="mdl-typography--font-light" href="#haut_de_page">
+							Retourner en haut de la page
+							<i class="material-icons">expand_less</i>
+						</a>
+					</div>
+				</div>
+				<div class="mdl-mega-footer--middle-section">
+					<p class="mdl-typography--font-light">
+						Contactez-nous pour sur 
+						<a class="mdl-navigation__link" href="https://github.com/projet-web-Maisonneuve/vino" target="_blank">GitHub</a> 
+						pour plus de fonctionnalités.
+					</p>
+				</div>
+			</footer>
+		</div>
+		<script src="<?php echo base_url(); ?>js/material.js"></script>
+	</body>
+</html>
