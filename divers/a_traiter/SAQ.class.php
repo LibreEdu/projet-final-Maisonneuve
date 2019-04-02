36e8d8a148b4fececfe3d972168da7a3c6a5267d
Alexandre Pachot <pachot@gmail.com>
2019-04-01 22:51:11 -0400

    SAQ.class (sprint 1)

--- divers/a_traiter/SAQ.class.php
+++ divers/a_traiter/SAQ.class.php
@@ -3,15 +3,11 @@
  * Class MonSQL
  * Classe qui génère ma connection à MySQL à travers un singleton
  *
- *
- * @author Alexandre Pachot et Fatemeh Homatash 
+ * @author Alexandre Pachot
+ * @author Fatemeh Homatash
  * @version 1.0
- *
- *
- *
  */
 class SAQ extends Modele {
-
 	const DUPLICATION = 'duplication';
 	const ERREURDB = 'erreurdb';
 
@@ -20,12 +16,12 @@ class SAQ extends Modele {
 	private $_stmt_bouteille_saq;
 	private $_stmt_type;
 
-	 public function __construct() {
+	public function __construct() {
 		parent::__construct();
 		try {
-			$this->_stmt_bouteille_saq = $this->_bd->prepare('INSERT INTO vino_bouteille_saq (libelle, millesime, id_type, pays, format, code_saq, prix) 
+			$this->_stmt_bouteille_saq = $this->_bd->prepare('INSERT INTO vino_bouteille_saq (nom, millesime, id_type, pays, format, code_saq, prix) 
 			VALUES (?, ?, ?, ?, ?, ?, ?)');
-			$this->_stmt_type = $this->_bd->prepare('INSERT INTO vino_type (libelle) VALUES (?)');
+			$this->_stmt_type = $this->_bd->prepare('INSERT INTO vino_type (type) VALUES (?)');
 		} catch (Exception $e) {
 			echo "Echec de la préparation : (" . $mysqli->errno . ") " . $mysqli->error;
 		}	
@@ -36,64 +32,6 @@ class SAQ extends Modele {
 	 * @param int $debut
 	 * @param int $nombre
 	 */
-	public function getProduits($debut = 0, $nombre = 100) {
-		// Initialisation du gestionnaire du client URL.
-		$gc = curl_init();
-
-		// URL à récupérer.
-		curl_setopt($gc, CURLOPT_URL, 'https://www.saq.com/webapp/wcs/stores/servlet/SearchDisplay?storeId=20002&searchTerm=vin&beginIndex=' . $debut . '&pageSize=' . $nombre);
-
-		// Retourne directement le transfert sous forme de chaine au lieu de l’afficher directement.
-		curl_setopt($gc, CURLOPT_RETURNTRANSFER, true);
-
-		// Pour que le php laisse accesse a https
-		curl_setopt($gc, CURLOPT_SSL_VERIFYPEER, false);
-
-		// Exécution de la session cURL.
-		self::$_pageweb = curl_exec($gc);
-
-		// Lecture du dernier code de réponse.
-		self::$_status = curl_getinfo($gc, CURLINFO_HTTP_CODE);
-
-		// Fermeture de la session.
-		curl_close($gc);
-
-		$doc = new DOMDocument();
-
-		// Activation du mode « recovery », c.-à-d. tentative d’analyser un document mal formé.
-		$doc->recover = true;
-
-		// Ne lance pas une DOMException en cas d’erreur.
-		$doc->strictErrorChecking = false;
-
-		// Chargement du code HTML à partir d’une chaîne de caractères (self::$_pageweb)
-		// @ : permet de ne pas afficher l’éventuel message d’erreur que pourrait retourner la fonction
-		@$doc->loadHTML(self::$_pageweb);
-
-		// Recherche tous les éléments qui ont une balise <div>
-		$elements = $doc->getElementsByTagName('div');
-
-		$nombreDeProduits = 0;
-
-		foreach ($elements as $noeud) {
-			if (strpos($noeud->getAttribute('class'), 'resultats_product') !== false) {
-				$info = self::recupereInfo($noeud);
-				//var_dump($info);die;
-				$retour = $this->ajouteProduit($info);
-				//var_dump($retour);die;
-				if ($retour->succes == false) {
-					echo "erreur : " . $retour->raison . "<br>";
-					echo "<pre>";
-					//var_dump($info);
-					echo "</pre>";
-					echo "<br>";
-				} else {
-					$nombreDeProduits++;
-				}
-			}
-		}
-		return $nombreDeProduits;
-	}
 
 	private function get_inner_html($node) {
 		$innerHTML = '';
@@ -101,7 +39,6 @@ class SAQ extends Modele {
 		foreach ($children as $child) {
 			$innerHTML .= $child -> ownerDocument -> saveXML($child);
 		}
-
 		return $innerHTML;
 	}
 
@@ -155,8 +92,7 @@ class SAQ extends Modele {
 					}
 					break;
 			}
-		}
-		
+		}		
 		// Récupération du prix
 		$cellules = $noeud->getElementsByTagName("td");
 		foreach ($cellules as $cellule) {
@@ -168,45 +104,65 @@ class SAQ extends Modele {
 		return $info;
 	}
 
-	private function ajouteProduit($bte) {
+	private function ajoutProduit($bte) {
 		$retour = new stdClass();
-		$retour -> succes = false;
-		$retour -> raison = '';
+		$retour->succes = false;
+		$retour->raison = "";
 		$dernierId = "";
 
 		// Récupère le type reçu en paramètre 
-		$rangeeType = $this->_bd->query("SELECT id FROM vino_type WHERE libelle = '" . $bte->type . "'");
+		$rangeeType = $this->_bd->query("SELECT id_type FROM vino_type WHERE type = '" . $bte->type . "'");
 		
-		// Vérifier si les rangées ne sont pas vides		
-		if ($rangeeType->num_rows == 1 ) {	
+		// Vérifier si les rangées ne sont pas vides
+		if ($rangeeType->num_rows == 1 ) {
 			// Récupère le id de type de vin
 			$id_type = $rangeeType->fetch_assoc();
-			$id_type = $id_type['id'];
+			$id_type = $id_type['id_type'];
 
 		} else {
 			// Ajouter le type dans la table de type
-			$this->_stmt_type->bind_param("s", $bte->type);		
-			$this->_stmt_type->execute();			
+			$this->_stmt_type->bind_param("s", $bte->type);
+			$this->_stmt_type->execute();
 			$id_type = $this->_stmt_type->insert_id;
 		}
 		
 		// Récupère le code_saq pour vérifier après si il existe dans la table ou non
-		 $rangeeCodeSaq = $this->_bd->query("SELECT id FROM vino_bouteille_saq WHERE code_saq = '" . $bte->code_SAQ . "'");
+		 $rangeeCodeSaq = $this->_bd->query("SELECT id_bouteille_saq FROM vino_bouteille_saq WHERE code_saq = '" . $bte->code_SAQ . "'");
 
 		//Si le code_saq n'existe pas dans le tableau
-		if ($rangeeCodeSaq->num_rows < 1) {
-			
-			$this->_stmt_bouteille_saq->bind_param("siissii", $bte->nom, $bte->millesime, $id_type, $bte->pays, $bte->format, $bte->code_SAQ, $bte->prix);
-			echo "<br>dernier id insere apres cherche codeSAQ ".$id_type;
-
+		if ($rangeeCodeSaq->num_rows < 1) {			
+			$this->_stmt_bouteille_saq->bind_param("siissid", $bte->nom, $bte->millesime, $id_type, $bte->pays, $bte->format, $bte->code_SAQ, $bte->prix);
 			$retour->succes = $this->_stmt_bouteille_saq->execute();
-			echo "<br>dernier id insere apres execute ".$id_type; 
-
 		} else {
 			$retour->succes = false;
 			$retour->raison = self::DUPLICATION;
 		}
 	return $retour;
 	}
+
+	public function obtenirBouteillesSaq() {
+		$bouteillesSaq = array();
+		$resultat = $this->_bd->query("SELECT bs.id_bouteille_saq AS id,
+					bs.code_saq AS code_saq,
+					bs.prix AS prix,
+					bs.millesime AS millesime,
+					bs.pays AS pays,
+					bs.format AS format,
+					bs.nom AS nom,
+					t.type AS type
+					FROM vino_bouteille_saq bs
+					INNER JOIN vino_type t
+					ON bs.id_type = t.id_type
+					ORDER BY id");
+		//Verifie si il a recu une resultat, si oui il fait un fetch et les mets dans le tableau $resltat
+		if ($resultat->num_rows) {
+			while ($chaqueResultat = $resultat->fetch_assoc()) {
+				$bouteillesSaq[] = $chaqueResultat;
+			}
+		}
+		else {
+			throw new Exception("Erreur de requête sur la base de donnée", 1);
+		}
+		return $bouteillesSaq;
+	}
 }
-?>
