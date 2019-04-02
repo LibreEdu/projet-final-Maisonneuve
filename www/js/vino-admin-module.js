08d925bfe27f596dd931bd1b01cfb0acb99a411d
Alexandre Pachot <pachot@gmail.com>
2019-04-02 11:58:05 -0400

    JS : admin-module

--- www/js/vino-admin-module.js
+++ www/js/vino-admin-module.js
@@ -1,4 +1,12 @@
+/**
+ * @file Module VinoAdmin
+ * @author Alexandre Pachot
+ * @version 0.1
+ */
 var vinoAdmin = (function(){
+
+	'use strict';
+
 
 	/**
 	 * Le module vinoAdmin gère les fonctions de la page d’administration.
@@ -7,12 +15,25 @@ var vinoAdmin = (function(){
 	var obj = {};
 
 
-
 	/**
 	 * Démarre l’importation des bouteilles de vin de la SAQ.
 	 */
 	obj.importer = function() {
-		console.log('test');
+		window.location = 'index.php?importation&action=importer';
+		// let requete = new Request('index.php?importation&action=importer');
+		// fetch(requete)
+		// .then(response => {
+		// 	if (response.status === 200) {
+		// 		return response.text();
+		// 	} else {
+		// 		throw new Error('Erreur');
+		// 	}
+		// })
+		// .then(response => {
+		// 	console.log(response);
+		// }).catch(error => {
+		// 	console.error(error);
+		// });
 	}
 
 	return obj;
