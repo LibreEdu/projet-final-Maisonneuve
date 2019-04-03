ed5ffe7980843951604c52217cfee2b56efa5a81
FatemehHo <f.homatash@gmail.com>
2019-04-03 15:59:00 -0400

    regler le bug de btn supprimer bouteille

--- www/js/vino.js
+++ www/js/vino.js
@@ -76,7 +76,7 @@ window.addEventListener('load', function() {
 				fetch(requete)
 				.then(response => {
 					if (response.status === 200) {
-						return response.text();
+						return response.json();
 					} else {
 						throw new Error('Erreur');
 					}
