3029c0a0abe9ed1cee3507719b54cce6840860dd
charefeddine <48103950+charefeddine@users.noreply.github.com>
2019-04-04 16:44:28 -0400

    Ajustement header et footer et css

--- prive/vues/modeles/bas-de-page.php
+++ prive/vues/modeles/bas-de-page.php
@@ -1,35 +1,12 @@
+		
 		<footer class="mdl-mega-footer">
-          <div class="mdl-mega-footer--middle-section">
-            <div class="mdl-mega-footer--drop-down-section">
-              <h3 class="mdl-mega-footer--heading">José Ignacio Delgado</h3>
+             <div>
+              <p> © Collége Maisonneuve - Projet web 2 : José Ignacio Delgado, Fatemeh Homatash, Alexandre Pachot, Charef Eddine Yagoubi</p>
             </div>
 
-            <div class="mdl-mega-footer--drop-down-section">
-              <h3 class="mdl-mega-footer--heading">Fatemeh Homatash</h3>
-            </div>
-
-            <div class="mdl-mega-footer--drop-down-section">
-              <h3 class="mdl-mega-footer--heading">Alexandre Pachot</h3>
-            </div>
-
-            <div class="mdl-mega-footer--drop-down-section">
-              <h3 class="mdl-mega-footer--heading">Charef Eddine Yagoubi</h3>
-            </div>
-
-             <div class="mdl-mega-footer--drop-down-section">
-              <h3 class="mdl-mega-footer--heading"> © Collége Maisonneuve - Projet web 2</h3>
-            </div>
-
-
-          </div>
-         <!--  <div class="mdl-mega-footer--bottom-section">
-            <div class="mdl-logo" id="college">
-              © Collége Maisonneuve Projet web 2
-            </div>
-          </div> -->
         </footer>
       </main>
-		<!-- </div> -->
+      
 		<script src="<?php echo base_url(); ?>js/material.js"></script>
 		<script src="<?php echo base_url(); ?>js/vino.js"></script>
 	</body>
