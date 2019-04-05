8fa9ea913c29d522a8a570240fef25eeae55a2f2
Alexandre Pachot <pachot@gmail.com>
2019-04-05 03:46:05 -0400

    Vue bas de page

--- prive/vues/modeles/bas-de-page.php
+++ prive/vues/modeles/bas-de-page.php
@@ -1,13 +1,11 @@
-		
-		<footer class="mdl-mega-footer">
-             <div>
-              <p> © Collége Maisonneuve - Projet web 2 : José Ignacio Delgado, Fatemeh Homatash, Alexandre Pachot, Charef Eddine Yagoubi</p>
-            </div>
+			<footer class="mdl-mega-footer">
+				<div>
+					<p> © Collège Maisonneuve - Projet web 2 : José Ignacio Delgado, Fatemeh Homatash, Alexandre Pachot, Charef Eddine Yagoubi</p>
+				</div>
 
-        </footer>
-      </main>
-      
-		<script src="<?php echo base_url(); ?>js/material.js"></script>
-		<script src="<?php echo base_url(); ?>js/vino.js"></script>
+			</footer>
+		</main>
+	<script src="<?php echo base_url(); ?>js/material.js"></script>
+	<script src="<?php echo base_url(); ?>js/vino.js"></script>
 	</body>
 </html>
