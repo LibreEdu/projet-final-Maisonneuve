966868b5a4cf37bdc49a251213f6df7e5e5a514e
Alexandre Pachot <pachot@gmail.com>
2019-04-05 09:07:31 -0400

    Vino veritas

--- prive/vues/modeles/menu-login.php
+++ prive/vues/modeles/menu-login.php
@@ -6,7 +6,7 @@
 			<header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
 				<div class="mdl-layout--large-screen-only mdl-layout__header-row"></div>
 				<div class="mdl-layout--large-screen-only mdl-layout__header-row">
-					<h3>Vino &amp; plaisir</h3>
+					<h3>Vino Veritas</h3>
 				</div>
 				<div class="mdl-layout--large-screen-only mdl-layout__header-row"></div>
 			</header>
