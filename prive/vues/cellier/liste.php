08b04912b8a1930e21ee05d58aea5b9c217dead1
José Ignacio Delgado <dgedit7@hotmail.com>
2019-04-05 15:15:58 -0400

    màj css

--- prive/vues/cellier/liste.php
+++ prive/vues/cellier/liste.php
@@ -10,7 +10,7 @@
 				foreach ($donnees["celliers"] as $cellier) {
 			?>		
 			<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp aaa">
-				<header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white ccc">
+				<header class="cellier_header section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white ccc">
 					<img src="images/cellier-vin.jpg" alt="" />
 				</header>			
 				<div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone bbb">
