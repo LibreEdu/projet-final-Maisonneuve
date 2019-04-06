2b3903828736f44b521e7de5bc92d73d085b4b6b
Alexandre Pachot <pachot@gmail.com>
2019-04-06 09:38:31 -0400

    Contrôleur bouteille

--- prive/controleurs/Controleur_Bouteille.php
+++ prive/controleurs/Controleur_Bouteille.php
@@ -7,18 +7,31 @@
  * @author   José Ignacio Delgado
  * @version  1.0
  */
- class Controleur_Bouteille extends Controleur
+final class Controleur_Bouteille extends Controleur
 {
 	/**
 	 * @var object $modele_bouteille Le modèle Modele_Bouteille.
-	 * @var object $modele_cellier Le modèle Modele_Cellier.
-	 * @var object $modele_type Le modèle Modele_Type.
 	 */
 	private $modele_bouteille;
+
+
+	/**
+	 * @var object $modele_cellier Le modèle Modele_Cellier.
+	 */
 	private $modele_cellier;
+
+
+	/**
+	 * @var object $modele_type Le modèle Modele_Type.
+	 */
 	private $modele_type;
-	
-	// Constructeur des modèles
+
+
+	/**
+	 * Initialisation des modèles.
+	 * 
+	 * @return void
+	 */
 	public function __construct()
 	{
 		$this->modele_bouteille = $this->modele('Modele_bouteille');
@@ -26,6 +39,14 @@
 		$this->modele_type = $this->modele('Modele_type');
 	}
 
+
+	/**
+	 * Récupère la requête de l’utilisateur afin de la traiter.
+	 * 
+	 * @param array $params Requête de l'utilisateur ($_REQUEST).
+	 * 
+	 * @return void
+	 */
 	public function traite(array $params)
 	{
 		// On vérifie que l’usagé est bien connecté
@@ -77,104 +98,82 @@
 		}
 	}
 
+
 	/**
-	 * Modifier une bouteille dans la table vino_bouteille 
-	 * Renvoyer au formulaire de modification
-	 * @param integer $_SESSION['id_usager'] L'id de l'usager connecté
-	 * @param integer $idBouteille L'id de la bouteille
-	 * @return array tous les données de la bouteille trouvée dans le catalogue
-	 * @return array tous les données des celliers trouvés dans le catalogue
-	 * @return array tous les données des types de vin trouvés dans le catalogue
+	 * Affiche le formulaire de modification d’une bouteille.
+	 * 
+	 * @return void
 	 */
-	public function modifier_form()
+	private function modifier_form()
 	{
 		$idBouteille = $this->modele_bouteille->appartient($_GET['id'],$_SESSION['id_usager']);
 
 		if ($idBouteille == null) {
 			header('Location: ' . site_url('login&action=logout') );
 		}
+
 		$donnees['id_cellier'] = $_GET['id_cellier'];
 		$donnees['bouteille'] = $this->modele_bouteille->obtenir_par_id($_GET['id']);
 		$donnees['types'] = $this->modele_type->obtenir_tous();
 		$donnees['celliers'] = $this->modele_cellier->obtenir_par_usager($_SESSION['id_usager']);
+
 		// Titre à afficher dans le formulaire
 		$donnees['titre'] = 'Modifier bouteille';
+
 		// Action du bouton input du formulaire
 		$donnees['actionBouton'] = 'modifier';
+
 		// Value du bouton input du formulaire
 		$donnees['titreBouton'] = 'Modifier la bouteille';
+
 		// Classe du bouton input du formulaire
 		$donnees['classeBouton'] = 'mdl-button mdl-js-button mdl-button--raised';
+
 		$this->afficheVue('modeles/en-tete');
 		$this->afficheVue('modeles/menu-usager');
 		$this->afficheVue('bouteille/formulaire', $donnees);
 		$this->afficheVue('modeles/bas-de-page');
 	}
 
+
 	/**
-	 * Modifier une bouteille dans la table vino_bouteille 
-	 * @param integer $_POST['id_cellier'] L'id cu cellier
+	 * Modifie une bouteille dans la table vino_bouteille.
+	 * 
+	 * @return void
 	 */
-	public function modifier()
+	private function modifier()
 	{
-		if($_POST['date_achat'] == '')
-		{
-			$_POST['date_achat'] = null;
-		}
-		else {
-			$_POST['date_achat'] = $_POST['date_achat'];
-		}
-		if($_POST['boire_avant'] == '')
-		{
-			$_POST['boire_avant'] = null;
-		}
-		else {
-			$_POST['boire_avant'] = $_POST['boire_avant'];
-		}
-		if($_POST['millesime'] == '')
-		{
-			$_POST['millesime'] = null;
-		}
-		else {
-			$_POST['millesime'] = $_POST['millesime'];
-		}
+		$_POST['date_achat'] = ($_POST['date_achat'] == '') ? null : $_POST['date_achat'];
+		$_POST['boire_avant'] = ($_POST['boire_avant'] == '') ? null : $_POST['boire_avant'];
+		$_POST['millesime'] = ($_POST['millesime'] == '') ? null : $_POST['millesime'];
+
 		$this->modele_bouteille->modifier();
 		header('Location: ' . site_url( 'cellier&action=voir&id_cellier=' . $_POST['id_cellier']) );
 	}
 
+
 	/**
-	 * Ajouter une bouteille dans la table vino_bouteille 
-	 * Renvoyer à la page d'affichage des celliers
-	 * @param integer $_POST['id_cellier'] L'id du cellier
+	 * Ajoute une bouteille dans la table vino_bouteille 
+	 * 
+	 * @return void
 	 */
-	public function ajouter()
+	private function ajouter()
 	{
-		if($_POST['date_achat'] == '')
-		{
-			$_POST['date_achat'] = null;
-		}
-		else {
-			$_POST['date_achat'] = $_POST['date_achat'];
-		}
-		if($_POST['boire_avant'] == '')
-		{
-			$_POST['boire_avant'] = null;
-		}
-		else {
-			$_POST['boire_avant'] = $_POST['boire_avant'];
-		}
-		if($_POST['millesime'] == '')
-		{
-			$_POST['millesime'] = null;
-		}
-		else {
-			$_POST['millesime'] = $_POST['millesime'];
-		}
+		$_POST['date_achat'] = ($_POST['date_achat'] == '') ? null : $_POST['date_achat'];
+		$_POST['boire_avant'] = ($_POST['boire_avant'] == '') ? null : $_POST['boire_avant'];
+		$_POST['millesime'] = ($_POST['millesime'] == '') ? null : $_POST['millesime'];
+
 		$this->modele_bouteille->ajouter();
 		header('Location: ' . site_url( 'cellier&action=voir&id_cellier=' . $_POST['id_cellier']) );
 	}
 
-	public function boire_js()
+
+	/**
+	 * Diminue la quantité de bouteilles de 1.
+	 * 
+	 * @return void
+	 */
+	private function boire_js()
 	{
 		$body = json_decode(file_get_contents('php://input'));
 		$this->modele_bouteille->modifier_quantite($body->id,-1);
@@ -182,7 +181,12 @@
 		echo json_encode($resultat);
 	}
 
-	public function ajouter_js()
+	/**
+	 * Augmente la quantité de bouteilles de 1.
+	 * 
+	 * @return void
+	 */
+	private function ajouter_js()
 	{
 		$body = json_decode(file_get_contents('php://input'));
 		$this->modele_bouteille->modifier_quantite($body->id, 1);
@@ -190,14 +194,13 @@
 		echo json_encode($resultat);
 	}
 
+
 	/**
-	 * Ajouter une bouteille dans la table vino_bouteille 
-	 * Renvoyer au formulaire d'ajout
-	 * @param integer $_SESSION['id_usager'] L'id de l'usager connecté
-	 * @return array tous les données des celliers trouvés dans le catalogue
-	 * @return array tous les données des types de vin trouvés dans le catalogue
+	 * Ajoute une bouteille dans la table vino_bouteille.
+	 * 
+	 * @return void
 	 */
-	public function ajouter_form()
+	private function ajouter_form()
 	{
 		$donnees['id_cellier'] = $_GET['id_cellier'];
 		$donnees['types'] = $this->modele_type->obtenir_tous();
@@ -216,26 +219,26 @@
 		$this->afficheVue('modeles/bas-de-page');
 	}
 
+
 	/**
-	 * Rechercher une bouteille dans la table vino_bouteille_saq par nom saisi dans le formulaire d'ajout
-	 * @param $body->nom Le nom à rechercher
-	 * @return array tous les données de la bouteille trouvée dans le catalogue
+	 * Recherche une bouteille dans la table vino_bouteille_saq par nom saisi dans le formulaire d'ajout
+	 * 
+	 * @return void
 	 */
-	public function saisie_semi_automatique()
+	private function saisie_semi_automatique()
 	{
 		$body = json_decode(file_get_contents('php://input'));
 		$listeBouteilles = $this->modele_bouteille_saq->autocomplete($body->nom);
 		echo json_encode($listeBouteilles);
 	}
 
+
 	/**
 	 * Supprime une bouteille par id bouteille dans la table vino_bouteille
-	 * @param integer $body->id_bouteille_supprimer L'id de la bouteille à supprimer
 	 * 
-	 * À VÉRIFIER, CE QUE LA FONCTION RETOURNE
-	 * @return boolean Indique si la requête a correctement fonctionné.
+	 * @return void
 	 */
-	public function supprimer_bouteille()
+	private function supprimer_bouteille()
 	{
 		$body = json_decode(file_get_contents('php://input'));
 		$resultat = $this->modele_bouteille->supprimerBouteilleParIdBouteille($body->id_bouteille_supprimer);
